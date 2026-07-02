5790ffebc995fcea1cba61dcd669580016fca59a
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-07-02 12:45:02 -0300

    feat: adiciona os atributos e métodos principais

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -1,4 +1,45 @@
 package entities;
 
+
 public class Cliente {
+    private String nome;
+    private int id;
+    private String email;
+
+    public Cliente(String nome, int id, String email) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - Id: " + id +
+                " - Email: " + email;
+    }
 }
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,52 @@
 package entities;
 
 public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
 
+    public Produto(int id, String nome, double preco, int quantidadeEstoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Produto produto = (Produto) o;
+        return id == produto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id +
+                " - " + nome +
+                " - " + String.format("%.2f", preco) +
+                " - Quantidade: " + quantidadeEstoque;
+    }
 }
