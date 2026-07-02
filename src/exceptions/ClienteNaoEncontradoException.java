9e8c9a5483b7ff34a4932c15583d2c5a98479a05
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-07-02 12:32:00 -0300

    feat: exceções personalizadas foram criadas

---
+++ src/exceptions/ClienteNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ClienteNaoEncontradoException extends RuntimeException {
+    public ClienteNaoEncontradoException(String message) {
+        super(message);
+    }
+}
